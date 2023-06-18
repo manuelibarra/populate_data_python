90c103bbe30fc79eac35f3e9c8dc61c0588a9d64
Manuel Ibarra <manuelibarra@gmail.com>
2023-06-17 23:15:47 -0500

    solviind tab-spaces in python

--- gen_data.py
+++ gen_data.py
@@ -5,26 +5,26 @@ def msghola():
    return "Hola mundo...!!"
 
 def getrandomstring(length):
-    # choose from all lowercase letter
-    letters = string.ascii_uppercase
-    result_str = ''.join(random.choice(letters) for i in range(length))
-    return result_str
+  # choose from all lowercase letter
+  letters = string.ascii_uppercase
+  result_str = ''.join(random.choice(letters) for i in range(length))
+  return result_str
 
 def generate_random_int(start,end):
-    # choose from all lowercase letter
-    return random.randint(start, end)
+  # choose from all lowercase letter
+  return random.randint(start, end)
 
 def generate_random_float(start,end):
-    # choose from all lowercase letter
-    return round(random.uniform(start, end),2)
+  # choose from all lowercase letter
+  return round(random.uniform(start, end),2)
 
 def generate_dni():
-    numbers = string.digits
-    result_dni =''
-    #print(numbers)
-    for i in range(1,9,1):# FOR desde 1 hasta 8, i++
-        result_dni = result_dni+(random.choice(numbers)) 
-    return result_dni   
+  numbers = string.digits
+  result_dni =''
+  #print(numbers)
+  for i in range(1,9,1):# FOR desde 1 hasta 8, i++
+      result_dni = result_dni+(random.choice(numbers)) 
+  return result_dni   
 
 def generate_province(number):
   if (number==1): 
