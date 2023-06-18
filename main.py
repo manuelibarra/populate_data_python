c4e35f83ce6d6515ff473d1a62e8b771bf88baf4
Manuel Ibarra <manuelibarra@gmail.com>
2023-06-17 21:00:59 -0500

    main incluido

---
+++ main.py
@@ -0,0 +1,10 @@
+from gen_data import *
+
+def main():
+    num=input("ingrese numero de 1 a 7:")
+    #5print(int(num))
+    print(generate_province((num)))
+
+if __name__ == "__main__":
+    main()
+
